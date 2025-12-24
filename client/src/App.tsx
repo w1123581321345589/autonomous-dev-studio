f590a7bb6b572f7270ce55139612092efadb0326
Will Rose <77295571+w1123581321345589@users.noreply.github.com>
2025-12-24 08:55:00 -0500

    Add client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,39 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { useWebSocket } from "@/hooks/use-websocket";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/Home";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function AppContent() {
+  useWebSocket();
+  return (
+    <>
+      <Toaster />
+      <Router />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <AppContent />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
